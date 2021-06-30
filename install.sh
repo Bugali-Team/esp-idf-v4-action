de30c345bcb01878cc793df5cce71b6dddc0e2ef
Yohann <yohannassouline@hotmail.fr>
2021-06-30 11:03:37 +0200

    clean action and add esp-adf

--- install.sh
+++ install.sh
@@ -1,40 +1,26 @@
 #!/bin/bash
 
-esp_idf_version="$1"
+URL_ESP_IDF=https://github.com/Bugali-Team/esp-idf
+URL_ESP_ADF=https://github.com/Bugali-Team/esp-adf
 
 echo -e "\n## Installing prerequisites"
 
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends tzdata
 
-case $esp_idf_version in
-  latest | v4.2 | v4.2.* | release/v4.2)
-    sudo apt-get install -y git wget flex bison gperf python3 python3-pip python3-setuptools python-is-python3 python3-libusb1 cmake ninja-build ccache libffi-dev libssl-dev dfu-util
-    ;;
-  v4.1 | v4.1.* | release/v4.1)
-    sudo apt-get install -y git wget flex bison gperf python3 python3-pip python3-setuptools python-is-python3 python3-libusb1 cmake ninja-build ccache libffi-dev libssl-dev
-    ;;
-  v4.0.* | release/v4.0)
-    sudo apt-get install -y git wget libncurses-dev flex bison gperf python3 python3-pip python3-setuptools python-is-python3 python3-libusb1 cmake ninja-build ccache libffi-dev libssl-dev
-    ;;
-  *)
-    echo "Unimplemented ESP-IDF version !"
-    exit 1
-    ;;
-esac
+sudo apt-get install -y git wget flex bison gperf python3 python3-pip python3-setuptools python-is-python3 python3-libusb1 cmake ninja-build ccache libffi-dev libssl-dev dfu-util
 
 mkdir ~/esp
 cd ~/esp
 
-case $esp_idf_version in
-  latest)
-    git clone --recursive https://github.com/espressif/esp-idf.git
-    ;;
-  *)
-    git clone -b $esp_idf_version --recursive https://github.com/espressif/esp-idf.git
-    ;;
-esac
+# install esp idf
+git clone --recursive $URL_ESP_IDF
 
 cd ~/esp/esp-idf
 
 echo -e "\n## Installing tools"
 ./install.sh > ~/esp-idf-install.log 2>&1
+
+cd ~/esp
+
+# install esp adf
+git clone --recursive $URL_ESP_ADF
