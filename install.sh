32fd5ac44d08cb5ee443512ef18fa77a7e6ee3f0
Yohann <yohannassouline@hotmail.fr>
2021-07-02 11:05:38 +0200

    clean install script

--- install.sh
+++ install.sh
@@ -10,17 +10,13 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends t
 sudo apt-get install -y git wget flex bison gperf python3 python3-pip python3-setuptools python-is-python3 python3-libusb1 cmake ninja-build ccache libffi-dev libssl-dev dfu-util
 
 mkdir ~/esp
-cd ~/esp
 
 # install esp idf
-git clone --recursive $URL_ESP_IDF
-
-cd ~/esp/esp-idf
-
-echo -e "\n## Installing tools"
-./install.sh > ~/esp-idf-install.log 2>&1
-
 cd ~/esp
+git clone --recursive $URL_ESP_IDF
+cd ~/esp/esp-idf
+./install.sh 2>&1
 
 # install esp adf
+cd ~/esp
 git clone --recursive $URL_ESP_ADF
